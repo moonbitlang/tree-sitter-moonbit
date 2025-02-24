ad6d2e3988fbfb9ffbc735f162ec7a47bca392ab
Haoxiang Fei <tonyfettes@tonyfettes.com>
2025-02-24 19:17:56 +0800

    chore: update bindings

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterMoonbit",
+    products: [
+        .library(name: "TreeSitterMoonbit", targets: ["TreeSitterMoonbit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
+    targets: [
+        .target(
+            name: "TreeSitterMoonbit",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+                // NOTE: if your language has an external scanner, add it here.
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterMoonbitTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterMoonbit",
+            ],
+            path: "bindings/swift/TreeSitterMoonbitTests"
+        )
+    ],
+    cLanguageStandard: .c11
+)
