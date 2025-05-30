16242a7b97d074ec32c64a4994d2ddee6a67cf5a
Haoxiang Fei <tonyfettes@tonyfettes.com>
2025-05-30 11:15:04 +0800

    fix(web): load React on DOMContentLoaded

--- web/index.tsx
+++ web/index.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import ThemeToggle from "./components/ThemeToggle";
 
-ReactDOM.createRoot(document.getElementById("theme-toggle")!).render(
-  <React.StrictMode>
-    <ThemeToggle />
-  </React.StrictMode>
-);
+window.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.createRoot(document.getElementById("theme-toggle")!).render(
+    <React.StrictMode>
+      <ThemeToggle />
+    </React.StrictMode>
+  );
+});
