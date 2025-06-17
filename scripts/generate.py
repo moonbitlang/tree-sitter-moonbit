d454e3902455e50cb171ed4c94ef4d75c67b9b39
Haoxiang Fei <tonyfettes@tonyfettes.com>
2025-06-17 11:50:20 +0800

    fix(ci): build all grammars

---
+++ scripts/generate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import subprocess
+import json
+from pathlib import Path
+
+
+def main():
+    tree_sitter = json.loads((Path(".") / "tree-sitter.json").read_text())
+
+    processes = []
+    for grammar in tree_sitter["grammars"]:
+        process = subprocess.Popen(["tree-sitter", "generate"], cwd=grammar["path"])
+        processes.append(process)
+
+    for process in processes:
+        process.wait()
+
+
+if __name__ == "__main__":
+    main()
